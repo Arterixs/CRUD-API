e7768bbdfdd083080922dc25f03b38bbf51ea0d8
Arterixs <artemontugg@gmail.com>
2023-07-03 02:32:54 +0300

    fix: correct type parametrs midleware

--- src/middleware/middlewareResponse.ts
+++ src/middleware/middlewareResponse.ts
@@ -1,7 +1,7 @@
 import { ServerResponse, IncomingMessage } from 'node:http';
 import { StatusCode } from '../types/enum.js';
 
-export const MiddlewareResponse = (res: ServerResponse<IncomingMessage>, code: StatusCode, message: any = null) => {
+export const MiddlewareResponse = (res: ServerResponse<IncomingMessage>, code: StatusCode, message: unknown = null) => {
   res.writeHead(code, {
     'Content-type': 'application/json',
   });
