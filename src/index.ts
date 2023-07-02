236d4567d972eafb3952d6b3c15bb6e6d12fe404
Arterixs <artemontugg@gmail.com>
2023-07-02 16:46:18 +0300

    feat: add put logic

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ import { createServer } from 'node:http';
 import { Method, StatusCode } from './types/enum.ts';
 import { getResponse } from './responses/getResponse.ts';
 import { getUserResponse } from './responses/getUserResponse.ts';
-import { readBodyResponse } from './responses/readBodyResponse.ts';
 import { putUserResponse } from './responses/putUserResponse.ts';
 import { deleteUserResponse } from './responses/deleteUserResponse.ts';
 import { postResponse } from './responses/postResponse.ts';
@@ -21,11 +20,12 @@ const server = createServer((req, res) => {
           getUserResponse(res, clientId);
           break;
         case Method.PUT:
-          // readBodyResponse(req)
-          //   .then((post) => {
-          //     putUserResponse(post, clientId, res);
-          //   })
-          //   .catch((err) => console.log(err));
+          putUserResponse(clientId, res, req).catch(() => {
+            res.writeHead(StatusCode.SERVER_ERROR, {
+              'Content-type': 'application/json',
+            });
+            res.end(JSON.stringify({ message: 'Server error' }));
+          });
           break;
         case Method.DELETE:
           deleteUserResponse(res, clientId);
--- src/responses/putUserResponse.ts
+++ src/responses/putUserResponse.ts
@@ -1,19 +1,58 @@
 import { ServerResponse, IncomingMessage } from 'node:http';
 import { dataBase } from '../model/model.app.ts';
 import { StatusCode } from '../types/enum.ts';
-import { User } from '../types/interface.ts';
+import { ServerUser } from '../types/interface.ts';
+import { isValidUserObject } from '../helpers/isValidUserObject.ts';
+import { readBodyResponse } from './readBodyResponse.ts';
+import { isValidUserId } from '../helpers/isValidUserId.ts';
 
-export const putUserResponse = (post: User, clientId: string, res: ServerResponse<IncomingMessage>) => {
-  if (dataBase.checkData(Number(clientId))) {
-    dataBase.setData(post, Number(clientId));
-    res.writeHead(StatusCode.SUCCES, {
+export const putUserResponse = async (clientId: string, res: ServerResponse<IncomingMessage>, req: IncomingMessage) => {
+  try {
+    const checkId = isValidUserId(clientId);
+    if (checkId) {
+      if (dataBase.checkData(clientId)) {
+        const userObject = await readBodyResponse(req);
+        const isValid = isValidUserObject(userObject);
+        if (isValid) {
+          dataBase.setData(userObject as ServerUser, clientId);
+          res.writeHead(StatusCode.SUCCES, {
+            'Content-type': 'application/json',
+          });
+          res.end(JSON.stringify(userObject));
+        } else {
+          res.writeHead(StatusCode.BAD_REQUEST, {
+            'Content-type': 'application/json',
+          });
+          res.end(JSON.stringify({ message: 'Invalid data in request' }));
+        }
+      } else {
+        res.writeHead(StatusCode.NOT_FOUND, {
+          'Content-type': 'application/json',
+        });
+        res.end(JSON.stringify({ message: 'User is not exsist' }));
+      }
+    } else {
+      res.writeHead(StatusCode.BAD_REQUEST, {
+        'Content-type': 'application/json',
+      });
+      res.end(JSON.stringify({ message: 'UserId is not valid' }));
+    }
+  } catch (err) {
+    res.writeHead(StatusCode.SERVER_ERROR, {
       'Content-type': 'application/json',
     });
-    res.end(JSON.stringify(post));
-  } else {
-    res.writeHead(StatusCode.NOT_FOUND, {
-      'Content-type': 'application/json',
-    });
-    res.end(JSON.stringify({ message: 'User is not found' }));
+    res.end(JSON.stringify({ message: 'Invalid data in request' }));
   }
+  // if (dataBase.checkData(clientId)) {
+  //   dataBase.setData(post, Number(clientId));
+  //   res.writeHead(StatusCode.SUCCES, {
+  //     'Content-type': 'application/json',
+  //   });
+  //   res.end(JSON.stringify(post));
+  // } else {
+  //   res.writeHead(StatusCode.NOT_FOUND, {
+  //     'Content-type': 'application/json',
+  //   });
+  //   res.end(JSON.stringify({ message: 'User is not found' }));
+  // }
 };
